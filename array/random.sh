729add0f0755e3a14aab9cbe9d4a9c0b5fb082cc
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 16:46:16 +0530

    ADDED : Second largest and Second smallest

---
+++ array/random.sh
@@ -0,0 +1,37 @@
+
+max=99
+min=1000
+secondmax=99
+secondmin=1000
+for (( counter=1; counter<=10; counter++))
+do
+array[$counter]=$((RANDOM%900+100))
+
+	if [ ${array[counter]} -gt $max ]
+	then
+	max=${array[counter]}
+fi
+        if [ ${array[counter]} -lt $min ]
+        then
+	min=${array[counter]}
+fi
+done
+echo ${array[@]} 
+
+
+#Second min max highest
+for (( counter=1; counter<=10; counter++))
+do
+if [ ${array[counter]} -gt $secondmax -a ${array[counter]} -ne $max ]
+        then
+        secondmax=${array[counter]}
+fi
+if [ ${array[counter]} -lt $secondmin -a ${array[counter]} -ne $min ]
+then
+        secondmin=${array[counter]}
+fi
+done
+
+echo  Second Minimum :$secondmin
+echo Second Maximum  :$secondmax
+
