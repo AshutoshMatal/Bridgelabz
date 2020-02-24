94a478871db7b79ab0d26f2411139f89436ff5e4
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-24 17:53:44 +0530

    ADDED : Showing the single digit in words

---
+++ case/singledigitcase.sh
@@ -0,0 +1,35 @@
+ echo “Enter a number”
+ read num
+ case $num in
+ [0])
+ echo “zero”
+ ;;
+ [1])
+ echo “one”
+ ;;
+ [2])
+ echo “two”
+ ;;
+ [3])
+ echo “three”
+ ;;
+[4])
+ echo “four”
+ ;;
+[5])
+ echo “five”
+ ;;
+[6])
+ echo “six”
+ ;;
+[7])
+ echo “seven”
+ ;;
+[8])
+ echo “eight”
+ ;;
+[9])
+ echo “nine”
+ ;;
+
+ esac
